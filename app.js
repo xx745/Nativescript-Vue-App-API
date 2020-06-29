46c05b7019c0ea6fa292c5940f84fbc22c955d94
Paweł Piegutkowski <ppiegut@gmail.com>
2020-06-29 22:57:10 +0100

    connected to the database

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/.env' });
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
 const PORT = process.env.PORT || 8082;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?ssl=trueretryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
+  console.log("Connected successfully to database!");
+
+  client.close();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
