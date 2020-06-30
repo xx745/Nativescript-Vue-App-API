72a9c6fccee62910cacaf3fa6394732546ee44b5
Paweł Piegutkowski <ppiegut@gmail.com>
2020-06-30 12:22:26 +0100

    added babel imports to connectDB

--- connectDB.js
+++ connectDB.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const dotenv = require('dotenv');
+import path from 'path';
+import dotenv from 'dotenv';
 dotenv.config({ path: path.join(__dirname, '/config/.env') });
-const MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from 'mongodb';
 const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority&tls=true`;
 const dbClient = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
