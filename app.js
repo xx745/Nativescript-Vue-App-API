c1a1f3c5449215395e3d99f3737390ff6771a5bf
Paweł Piegutkowski <ppiegut@gmail.com>
2020-06-30 11:46:47 +0100

    removed express-csp-header

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: path.join(__dirname, '/config/.env') });
 const { connectDB, dbClient } = require('./connectDB');
 const express = require('express');
-const { expressCspHeader, NONCE, SELF } = require('express-csp-header');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8082;
 const dbName = process.env.DB_NAME;
@@ -12,12 +11,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressCspHeader({
-  directives: {
-    'default-src': [SELF],
-    'script-src': [NONCE]
-  }
-}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
