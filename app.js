f1600956911029b95d118ef47d86bf678e85c16d
Paweł Piegutkowski <ppiegut@gmail.com>
2020-06-30 10:22:56 +0100

    exported db connection config

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-const path = require("path");
+const path = require('path');
 const dotenv = require('dotenv');
-dotenv.config({ path: `${path.dirname()}/config/.env` });
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require('mongodb').MongoClient;
-
+dotenv.config({ path: path.join(__dirname, '/config/.env') });
+const { connectDB, dbClient } = require('./connectDB');
+const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8082;
-const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority&tls=true`;
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const dbClient = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/all-todos', async (req, res) => {
-  const data = await connectDB();
-  res.json(data);
+  await connectDB();
+  const db = dbClient.db(process.env.DB_NAME);
+  const col = db.collection(process.env.DB_COLLECTION)
+  const allItems = await col.find({}).toArray();
+
+  res.json(allItems);
 });
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
-
-async function connectDB() {
-  try {
-    await dbClient.connect();
-    console.log("Connected successfully to the database!");
-
-    const db = dbClient.db(process.env.DB_NAME);
-    const collection = db.collection(process.env.DB_COLLECTION);
-    const allTodos = await collection.find({}).toArray();
-    
-    return allTodos;
-
-  } catch (err) {
-    console.log(err.stack);
-  }
-
-  client.close();
-};
