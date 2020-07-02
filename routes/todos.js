8a152c34767b6c248bdee3c406b8e1c278c35169
Paweł Piegutkowski <ppiegut@gmail.com>
2020-07-02 09:36:39 +0100

    added a route to add todos

--- routes/todos.js
+++ routes/todos.js
@@ -8,12 +8,34 @@ const dbName = process.env.DB_NAME;
 const dbCollection = process.env.DB_COLLECTION;
 
 router.get('/', async (req, res) => {
-  await connectDB();
-  const db = dbClient.db(dbName);
-  const col = db.collection(dbCollection)
-  const allTodos = await col.find({}).toArray();
+  try {
+    await connectDB();
+    const db = dbClient.db(dbName);
+    const col = db.collection(dbCollection)
+    const allTodos = await col.find({}).toArray();
+    res.json(allTodos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-  res.json(allTodos);
+router.post('/add-todo', async (req, res) => {
+  try {
+    await connectDB();
+    const db = dbClient.db(dbName);
+    const col = db.collection(dbCollection);
+    const newTodo = { todoText: req.body.todoText };
+
+    col.insertOne(newTodo, (err, result) => {
+      if (result) console.log(`Inserted ${result.insertedCount} todo successfully!`);
+      if (err) console.error(err);
+    });
+
+    res.status(201).json(newTodo);
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = router;
