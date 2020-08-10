8c57a286fa2fd4a14586589e72bbf43e3debfec3
Paweł Piegutkowski <ppiegut@gmail.com>
2020-08-10 12:25:49 +0100

    added few rules to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,21 +9,21 @@ module.exports = {
     'ecmaVersion': 11
   },
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-eval': 'error',
+    'no-multi-spaces': 'error',
+    'capitalized-comments': 'error',
+    'eol-last': ['error', 'always'],
+    'no-dupe-keys': 'error',
+    'no-irregular-whitespace': 'error',
+    // 'no-magic-numbers': ['error', 
+    //   { 'ignore': [1, 200, 201, 400, 404, 500] } // Can be extended by other HTTP status codes
+    // ],
+    'camelcase': 'error',
+    'max-len': ['error', { 'code': 120 }]
+
   }
 };
