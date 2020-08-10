0ea5955e6a8b11caa577380b5dad6978ed4e2100
Paweł Piegutkowski <ppiegut@gmail.com>
2020-08-10 17:42:47 +0100

    added routes for todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const ToDo = require('../mongoose_models/ToDo');
 
+router.get('/all', async (req, res) => {
+  await ToDo.find({})
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        'results': result
+      });
+    })
+    .catch(err => {
+      return res.status(404).json({
+        'error': err
+      });
+    });
+});
+
 router.post('/add', async (req, res) => {
   const newToDo = new ToDo({
     text: req.body.text,
@@ -9,10 +24,30 @@ router.post('/add', async (req, res) => {
 
   try {
     const savedToDo = await newToDo.save();
-    res.send(savedToDo);
-  } catch (error) {
-    res.status(400).send(error);
+    console.log(`Created todo ID: ${savedToDo._id}`);
+    res.status(200).json({
+      'saved_todo': savedToDo
+    });
+  } catch (err) {
+    res.status(400).json({
+      'error': err
+    });
   }
+});
+
+router.post('/delete/:todoId', async (req, res) => {
+  ToDo.deleteOne({ _id: req.params.todoId })
+    .then(result => {
+      res.status(200).json({
+        'deleted_todos': result.deletedCount
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        'todo_delete_error': err
+      });
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -23,13 +23,13 @@ router.post('/register', (req, res) => {
               password: hash
             });
             newUser.save()
-              .then((result) => {
+              .then(result => {
                 console.log(`Created user ID: ${result._id}`);
                 res.status(201).json({
                   'new_user_created': result
                 });
               })
-              .catch((err) => {
+              .catch(err => {
                 console.log(err);
                 res.status(500).json({
                   'user_error': err
@@ -41,14 +41,50 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          'user_login_issue': 'Auth failed or user does not exist'
+        });
+      }
+
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            'user_login_issue': 'Auth failed completely'
+          });
+        }
+
+        if (result) {
+          return res.status(200).json({
+            'user_login': 'Auth successful'
+          });
+        }
+
+        return res.status(401).json({
+          'user_login_issue': 'Auth failed'
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        'user_login_error': err
+      });
+    });
+});
+
 router.post('/delete/:userId', (req, res) => {
   User.deleteOne({ _id: req.params.userId })
-    .then((result) => {
+    .then(result => {
       res.status(200).json({
         'deleted_users': result.deletedCount
       });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.status(500).json({
         'user_delete_error': err
