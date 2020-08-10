5610c3a62c90b7ec1429654c165bae5e25369845
Paweł Piegutkowski <ppiegut@gmail.com>
2020-08-10 12:55:27 +0100

    added basic auth path & mongoose package

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  return res.send('home route is working');
+});
+
+const authRouter = require('./routes/auth');
+app.use('/auth', authRouter);
+
 const userRouter = require('./routes/user');
 app.use('/users', userRouter);
 
 const todosRouter = require('./routes/todos');
 app.use('/todos', todosRouter);
-
-app.get('/', (req, res) => {
-  return res.send('home route is working');
-});
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
