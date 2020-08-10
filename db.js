c8f244bd012fa60e189da3ad96d335611b54e354
Paweł Piegutkowski <ppiegut@gmail.com>
2020-08-10 15:42:54 +0100

    added email regex

--- db.js
+++ db.js
@@ -3,7 +3,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async() => {
-  mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+  mongoose.connect(
+    process.env.DB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    }
+  );
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'Databse connection error:'));
   db.once('open', function() {
--- mongoose_models/User.js
+++ mongoose_models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// eslint-disable-next-line
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -10,6 +13,8 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    match: emailRegEx,
     max: 255,
     min: 5
   },
