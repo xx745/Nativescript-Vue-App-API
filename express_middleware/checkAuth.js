8cc541b62e21cba7f218f730b13f9d807c38257b
Paweł Piegutkowski <ppiegut@gmail.com>
2020-08-11 10:04:01 +0100

    added token to the headers

--- express_middleware/checkAuth.js
+++ express_middleware/checkAuth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const decoded = jwt.verify(req.body.token, process.env.JWT_PRV_KEY, null);
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.JWT_PRV_KEY, null);
     req.userData = decoded;
     next();
   } catch (err) {
